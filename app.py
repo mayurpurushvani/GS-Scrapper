d2895f11e07121bffb97dc5b95c3ff84337f49f5
Darshan Tandel <darshantandel1998@gmail.com>
2023-10-31 23:09:35 +0530

    code updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
-import sys
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from product import Product
 
@@ -9,37 +9,29 @@ from product import Product
 class MyProcess:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--blink-settings=imagesEnabled=false')
         # chrome_options.add_experimental_option('detach', True)
-        chrome_options.add_argument('--start-maximized')
 
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def run(self):
-        self.scrapeProduct('https://www.amazon.com/dp/B0CC2GD4D9')
-        # self.driver.get('https://www.amazon.com/s?k=laptops')
-        #
-        # elements = self.driver.find_elements(By.CSS_SELECTOR, '.s-result-item[data-component-type="s-search-result"]')
-        # for element in elements:
-        #     aTag = element.find_element(By.CSS_SELECTOR, '[data-component-type="s-product-image"] a')
-        #     href = aTag.get_attribute('href')
-        #     self.scrapeProduct(href)
+        self.driver.get('https://www.amazon.com/s?k=laptops')
 
-    def scrapeProduct(self, href):
-        # self.driver.switch_to.new_window('tab')
+        products = self.driver.find_elements(By.CSS_SELECTOR, '.s-result-item[data-component-type="s-search-result"]')
+        for product in products:
+            href = product.find_element(By.CSS_SELECTOR, '[data-component-type="s-product-image"] a').get_attribute(
+                'href')
+            self.scrape_product(href)
+
+    def scrape_product(self, href):
+        # TODO new tab
+        # TODO open variant wise product
         self.driver.get(href)
-
         product = Product(self.driver)
-        data = product.get_rating()
-        print(json.dumps(data, indent=4))
-        """
-            # TO DO open variant wise product 
-            # https://www.amazon.com/dp/B0CGTVW7ZX?th=1
-            
-            [Products related to this item ASIN]
-        """
-        sys.exit()
+        product_data = product.scrape()
+        print(json.dumps(product_data, indent=4))
 
     def __del__(self):
         self.driver.close()
--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,7 +14,7 @@ class Product:
         self.driver = driver
         self.data = {}
 
-    def get_name(self) -> str:
+    def get_name(self):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'productTitle'))).text
 
     def get_image_urls(self):
@@ -52,28 +53,57 @@ class Product:
     def get_technical_details_additional_information(self):
         return utils.table_to_json(self.driver, '#productDetails_detailBullets_sections1')
 
+    def get_review(self, review):
+        utils.expander_expand(review, '', 'Read more')
+
+        country = None
+        date = None
+        pattern = r'Reviewed in the (?P<country>.*) on (?P<date>.*)'
+        review_date = WebDriverWait(review, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="review-date"]'))).text
+        match = re.search(pattern, review_date)
+        if match:
+            country = match.group('country')
+            date = datetime.strptime(match.group('date'), '%B %d, %Y').strftime('%Y-%m-%d')
+
+        rating = None
+        pattern = r'(?P<rating>[0-9]*\.?[0-9]+) out of (?P<out_of>\d)'
+        review_star_rating = WebDriverWait(review, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="review-star-rating"]'))).get_attribute(
+            'textContent')
+        match = re.search(pattern, review_star_rating)
+        if match:
+            rating = float(match.group('rating'))
+
+        return {
+            'id': review.get_attribute('id'),
+            'customer_name': WebDriverWait(review, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '.a-profile-name'))).text,
+            'rating': rating,
+            'title': WebDriverWait(review, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="review-title"] span:nth-child(3)'))).text,
+            'country': country,
+            'date': date,
+            'description': WebDriverWait(review, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="review-collapsed"]'))).text
+        }
+
     def get_customer_reviews(self):
         customer_reviews = []
-        # {
-        #     customer { name }
-        #     review
-        #     title
-        #     country
-        #     date
-        #     description
-        # }
+        reviews = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[data-hook="review"]')))
+        for review in reviews:
+            customer_reviews.append(self.get_review(review))
         return customer_reviews
 
     def get_rating(self):
         final_rating = None
-        total_rating = None
         pattern = r'(?P<rating>[0-9]*\.?[0-9]+) out of (?P<out_of>\d)'
         rating_out_of_text = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="rating-out-of-text"]'))).text
         match = re.search(pattern, rating_out_of_text)
         if match:
             final_rating = float(match.group('rating'))
-            total_rating = int(match.group('out_of'))
 
         total_review_count = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '[data-hook="total-review-count"]'))).text
@@ -81,7 +111,6 @@ class Product:
 
         return {
             'final_rating': final_rating,
-            'total_rating': total_rating,
             'rating_details': {
                 '5': WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
                     (By.CSS_SELECTOR, '#histogramTable tr:nth-child(1) td:nth-child(3) a'))).text,
@@ -99,6 +128,7 @@ class Product:
         }
 
     def scrape(self):
+        # TODO [Products related to this item ASIN]
         self.data = {
             'name': self.get_name(),
             'image_urls': self.get_image_urls(),
