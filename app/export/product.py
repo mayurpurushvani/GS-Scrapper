bd35a77b85fefb61bf8dfa824c8a9d8668755361
mayur purushvani <mayurpurushvani@gmail.com>
2024-02-08 00:20:37 +0530

    GS: Attribute code modified

--- app/export/product.py
+++ app/export/product.py
@@ -24,27 +24,27 @@ class Product:
 
             # Set Data In Excel Array
             for p in products:
-                if len(p.attribute) > attributeHeaders:
-                    attributeHeaders = len(p.attribute)
+                if len(p.attributes) > attributeHeaders:
+                    attributeHeaders = len(p.attributes)
 
                 finalArray = (["external", p.sku, p.name, p.published, p.visibility_in_catalog,
                                p.short_description, p.description, p.in_stock, p.weight,
                                p.length, p.width, p.height, p.allow_customer_reviews,
                                p.sale_price, p.regular_price, p.categories, p.tags,
                                p.images, p.external_url, "Go To Store"])
-                for attr in p.attribute:
-                    finalArray += ([attr.attribute, attr.value])
+                for attr in p.attributes:
+                    finalArray += ([attr.attribute.title().replace('_', ' '), attr.value])
                 finalInsertArray.append(finalArray)
 
             # Set Header In Excel
             headers = ["Type", "SKU", "Name", "Published", "Visibility in catalog", "Short description",
-                       "Description", "In stock?", "Weight (unit)", "Length (unit)", "Width (unit)",
-                       "Height (unit)", "Allow customer reviews?", "Sale price", "Regular price", "Categories",
+                       "Description", "In stock?", "Weight (lbs)", "Length (in)", "Width (in)",
+                       "Height (in)", "Allow customer reviews?", "Sale price", "Regular price", "Categories",
                        "Tags", "Images", "External URL", "Button text"]
             if attributeHeaders != 0:
                 for row in range(1, attributeHeaders + 1):
                     headers.append('Attribute {0} name'.format(row))
-                    headers.append('Attribute {0} value'.format(row))
+                    headers.append('Attribute {0} value(s)'.format(row))
             csvwriter.writerow(headers)
 
             # Set Data In Excel Sheet
